55ba77cc767a8f2477948f72b4c556a4615e1f07
Elhassane Lahyani <elahyani@e2r8p6.1337.ma>
2021-03-11 15:57:15 +0100

    finish ex07

--- cpp_module_01/ex07/replace.cpp
+++ cpp_module_01/ex07/replace.cpp
@@ -5,11 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: elahyani <elahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/10 17:27:00 by elahyani          #+#    #+#             */
-/*   Updated: 2021/03/10 17:27:20 by elahyani         ###   ########.fr       */
+/*   Created: 2021/03/11 15:51:01 by elahyani          #+#    #+#             */
+/*   Updated: 2021/03/11 15:51:02 by elahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+
+std::string	change_filename(std::string filename)
+{
+	int		i;
+
+	i = 0;
+	while (filename[i])
+	{
+		filename[i] = std::toupper(filename[i]);
+		i++;
+	}
+	filename += ".replace";
+	return filename;
+}
+
+int main(int argc, char **argv) {
+
+	std::string	line;
+	std::string	oldw;
+	std::string	neww;
+	std::string filename;
+
+	if (argc != 4) {
+		std::cout << "error:\tbad arguments." << std::endl;
+		return (1);
+	}
+	oldw = argv[2];
+	neww = argv[3];
+	std::ifstream	ifs(argv[1]);
+	filename = change_filename(argv[1]);
+	std::ofstream	ofs(filename);
+	while (getline(ifs, line)) {
+		while (line.find(oldw) != std::string::npos)
+			line.replace(line.find(oldw), oldw.length(), neww);
+		ofs << line << std::endl;
+	}
+	ifs.close();
+	ofs.close();
+	return 0;
+}
